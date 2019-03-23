22064b39c6b6ed39c0a39e1d3b41ab8f06647a6e
matty <mathewgibney2@gmail.com>
2019-03-23 16:55:59 +0000

    Main template file

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+char * readFile(char filename[]){
+    File *inputFile;
+    char byteArray[1024]
+    inputFile = fopen(filename,"rb");    
+}
+
+int main(){
+    return 0;
+}
